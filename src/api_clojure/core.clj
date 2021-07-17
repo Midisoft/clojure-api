c0d9f42b8a30e8b884e12d5782fcd15fc289d09b
Midisoft <midisoft.corporation@gmail.com>
2021-07-17 20:29:48 -0300

    new commit

--- src/api_clojure/core.clj
+++ src/api_clojure/core.clj
@@ -1,7 +1,28 @@
 (ns api-clojure.core
-  (:gen-class))
+    (:require   [io.pedestal.http.route :as route]
+                [io.pedestal.http :as http]
+                [io.pedestal.test :as test]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+    (println "Server started"))
+
+(defn hello [request] {
+    :status 200 
+    :body (str "Hello, " (get-in request [:query-params :name] "Welcome!"))})
+
+(def routes (route/expand-routes
+                #{
+                    ["/hello" :get hello :route-name :main]
+                }))
+  
+(def service-map {
+    ::http/routes   routes
+    ::http/port     9999
+    ::http/type     :jetty
+    ::http/join?    false})
+  
+(def server (atom nil))
+
+(reset! server (http/start (http/create-server service-map)))
+
+(test/response-for (::http/service-fn @server) :get "/hello")
