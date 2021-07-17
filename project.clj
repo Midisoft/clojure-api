8afa63a179b951186f0fc314184c676cbdd5d86c
Midisoft <midisoft.corporation@gmail.com>
2021-07-17 18:12:30 -0300

    add dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,22 @@
 (defproject api-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot api-clojure.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {
+        :name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+        :url "https://www.eclipse.org/legal/epl-2.0/"
+    }
+    :dependencies [
+        [org.clojure/clojure "1.10.1"]
+        [io.pedestal/pedestal.service "0.5.9"]
+        [io.pedestal/pedestal.route   "0.5.9"]
+        [io.pedestal/pedestal.jetty   "0.5.9"]
+    ]
+    :main ^:skip-aot api-clojure.core
+    :target-path "target/%s"
+    :profiles {
+        :uberjar {
+            :aot :all
+        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+        }
+    })
+    
